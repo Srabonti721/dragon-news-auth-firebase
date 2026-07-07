88e75f7adfd389760e92e718696a6fdf258b943d
Srabonti721 <somayaaktarshabonti721@gmail.com>
2026-07-07 12:32:13 +0600

    added auth layout , login, register form and handle login, register

---
+++ src/pages/Login.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Login = () => {
+const handleLogin = e =>{
+    e.preventDefault();
+   
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log( email, password);
+    
+}
+    return (
+<div className='flex justify-center min-h-screen items-center'>
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <div className="card-body">
+        <h1 className="text-3xl font-bold">Login Your account!</h1>
+        <form onSubmit={handleLogin} className="fieldset">
+          <label className="label  text-black font-semibold">Email</label>
+          <input name='email' type="email" className="input" placeholder="Email Address" />
+          <label className="label  text-black font-semibold">Password</label>
+          <input name='password' type="password" className="input" placeholder="Password" />
+          <div><a className="link link-hover">Forgot password?</a></div>
+          <button className="btn btn-neutral mt-4">Login</button>
+          <p className='text-sm'>Don't have any account ? <Link className='text-blue-600 underline' to={'/auth/register'}>Register</Link></p>
+        </form>
+      </div>
+    </div>
+    </div>
+    );
+};
+
+export default Login;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Register = () => {
+    const handleRegister = e =>{
+            e.preventDefault();
+             const name = e.target.name.value;
+    const photo = e.target.photo.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    console.log(name, photo,email, password);
+    }
+    return (
+<div className='flex justify-center min-h-screen items-center'>
+    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+      <div className="card-body">
+        <h1 className="text-2xl font-bold">Register Your account!</h1>
+        <form onSubmit={handleRegister} className="fieldset">
+          <label className="label text-black font-semibold">Your Name</label>
+          <input name='name' type="text" className="input" placeholder=" Your Name" />
+          <label className="label  text-black font-semibold">Photo URL</label>
+          <input name='photo' type="text" className="input" placeholder="Photo URL" />
+          <label className="label  text-black font-semibold">Email</label>
+          <input name='email' type="email" className="input" placeholder="Email Address" />
+          <label className="label  text-black font-semibold">Password</label>
+          <input name='password' type="password" className="input" placeholder="Password" />
+          <div><a className="link link-hover">Forgot password?</a></div>
+          <button className="btn btn-neutral mt-4">Login</button>
+          <p className='text-sm'>Already have any account ? <Link className='text-blue-600 underline' to={'/auth/login'}>Login</Link></p>
+        </form>
+      </div>
+    </div>
+    </div>
+    );
+};
+
+export default Register;
